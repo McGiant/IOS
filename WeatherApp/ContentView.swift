45e74ddf56abe99c5d6211e99fd818b3424c482d
TKonga <tapiwa.konga@cassavasmartech.co.zw>
2021-02-13 21:33:18 +0200

    Initial Commit

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Tapiwa Konga on 13/2/2021.
+//  Copyright © 2021 Tapiwa Konga. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
